a5cbeee7d71aa9e5177ad846876feeb37d7919a4
atto <ak.tamalin@gmail.com>
2018-05-10 23:08:53 +0900

    add getPixel function

--- image.c
+++ image.c
@@ -53,3 +53,53 @@ void disposeImage(ImageData *img){
     free(img);
 }
 
+int  getPixel(ImageData *img, int x, int y, Pixel *pixel){
+    int return_value = 1;
+    int address;
+    int depth, gray_value;
+    BYTE *pixels;
+
+    if(img == NULL)
+        return -1;
+    if(img->pixels == NULL)
+        return -1;
+
+    if(x < 0){
+        x = 0;
+        return_value = 0;
+    }
+    if(x >= img->width){
+        x = img->width - 1;
+        return_value = 0;
+    }
+    if(y < 0){
+        y = 0;
+        return_value = 0;
+    }
+    if(y >= img->height){
+        y = img->height - 1;
+        return_value = 0;
+    }
+
+    depth = img->depth;
+    address = x + y*img->width;
+    pixels = img->pixels;
+
+    if(depth == 8){
+        gray_value = pixels[address];
+        pixel->r = gray_value;
+        pixel->g = gray_value;
+        pixel->b = gray_value;
+    }else if(depth == 24){
+        pixels += (address * 3);
+        pixel->r = (*pixels);
+        pixels++;
+        pixel->g = (*pixels);
+        pixels++;
+        pixel->b = (*pixels);
+    }else{
+        return -1;
+    }
+
+    return return_value;
+}
